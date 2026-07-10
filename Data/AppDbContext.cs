2e4de6c414bc1939b4744e1b4144beb75e061a0a
kistapuramsuchithra <kistapuramsuchithra434@gmail.com>
2026-07-10 17:15:03 +0530

    added data in main

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCTaskManagmentApp.Models;
+using System.Collections.Generic;
+
+namespace MVCTaskManagmentApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+        public DbSet<Users> Users { get; set; }
+    }
+}
