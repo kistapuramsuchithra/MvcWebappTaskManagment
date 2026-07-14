7a253a877f9d3c1984392a3ed47474d6f0fbe7eb
kistapuramsuchithra <kistapuramsuchithra434@gmail.com>
2026-07-14 12:23:05 +0530

    final changes

--- Program.cs
+++ Program.cs
@@ -1,28 +1,74 @@
+//using Microsoft.EntityFrameworkCore;
+//using MVCTaskManagmentApp.Data;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddSession();
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddHttpContextAccessor();
+//var app = builder.Build();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    db.Database.Migrate();
+//}
+//// Configure the HTTP request pipeline.
+////if (!app.Environment.IsDevelopment())
+////{
+////    app.UseExceptionHandler("/Home/Error");
+////    app.UseExceptionHandler("/Task/Index");
+////    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+////    app.UseHsts();
+////}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+//app.UseRouting();
+//app.UseSession();
+//app.UseAuthorization();
+
+//app.MapStaticAssets();
+
+////app.MapControllerRoute(
+////    name: "default",
+////    pattern: "{controller=Tasks}/{action=Index}/{id?}")
+////    .WithStaticAssets();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Account}/{action=Login}/{id?}")
+//     .WithStaticAssets();
+//app.Run();
 using Microsoft.EntityFrameworkCore;
 using MVCTaskManagmentApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddSession();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
-// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    app.UseExceptionHandler("/Task/Index");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -30,20 +76,17 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+
 app.UseRouting();
+
 app.UseSession();
+
 app.UseAuthorization();
-
-app.MapStaticAssets();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Tasks}/{action=Index}/{id?}")
-//    .WithStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}")
-     .WithStaticAssets();
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
 app.Run();
